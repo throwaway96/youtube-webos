559e95fdb2b9ab2366a75eead2742989ceb1a956
throwaway96 <68320646+throwaway96@users.noreply.github.com>
2024-03-27 00:33:54 -0400

    ui.js: remove tectonicConfig stuff
    Turns out this wasn't necessary because we're setting
    env_forceFullAnimation=1. It also probably wasn't even having any effect
    because it ran too early.
    
    The only thing that isn't set is enableOnScrollLinearAnimation, but I
    have no idea what that does.

--- src/ui.js
+++ src/ui.js
@@ -221,17 +221,6 @@ async function logoHideShow() {
 
 function applyUIFixes() {
   try {
-    const tc = window.tectonicConfig;
-    tc.clientData.legacyApplicationQuality = 'full-animation';
-    tc.featureSwitches.enableAnimations = true;
-    tc.featureSwitches.enableListAnimations = true;
-    tc.featureSwitches.enableOnScrollLinearAnimation = true;
-    tc.featureSwitches.isLimitedMemory = false;
-  } catch (e) {
-    console.error('error setting tectonicConfig:', e);
-  }
-
-  try {
     const bodyClasses = document.body.classList;
 
     const observer = new MutationObserver(function bodyClassCallback(
